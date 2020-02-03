b3bcecbbe10987943ad49b0f869a0fa331255a8c
Mustafa Elnaquib <>
2020-02-03 03:36:05 +0200

    initial commit AGPLv3 License

---
+++ README.rst
@@ -0,0 +1,37 @@
+Lieutenant Commander
+=====================
+
+Introduction
+-------------
+
+Lieutenant Commander shall try to help you talk Bioinformatics to Linux.
+Built for bioinformatics Enthusiasts;
+
+Features
+---------
+
+- **easy to use**
+- **easy to install**
+- **smart**
+- **hints**
+- **autocomplete**
+- **online help**
+- **...**
+
+Installation
+------------
+
+TODO
+
+
+Usage
+-----
+
+TODO
+
+Contribute
+----------
+
+https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow
+
+TODO
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lieutenant-commander-melnaquib", # Replace with your own username
+    version="0.0.1",
+    author="Mustafa Elnaquib",
+    author_email="melnaquib@gmail.com",
+    description="help talk to Linux for BioInformatics",
+    long_description=long_description,
+    url="https://github.com/melnaquib/ltcmdr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: AGPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
